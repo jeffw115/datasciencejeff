2c27850e8f1e248ab96727cff0997799a482090a
Jeff <51469474+rutgersjeff@users.noreply.github.com>
2023-08-05 12:31:01 -0400

    Update bank.py
    Made updates to bank.py file

--- bank.py
+++ bank.py
@@ -1,19 +1,20 @@
+# ATM Bank code
 print("Welcome to Jeff's Bank!")
-code = int(input("Please enter your 4-digit PIN number: "))
-pin = 1993
-attempts = 1
+code = int(input("Please enter your 4-digit PIN number: ")) # user to enter PIN number to log into account
+pin = 1993 # PIN number for login
+attempts = 1 # 1st login attempt
 
 # Incorrect code in first attempt
 if code != pin and attempts == 1:
     code = int(input("Sorry, you have entered an incorrect PIN number. You have 2 more attempts. Please try again: "))
-    attempts = attempts + 1 # Required to allow users to re-enter code until max attempts
+    attempts = attempts + 1 # Required to allow users to re-enter code until max attempts reached
 
 # Correct code in first attempt
 if code == pin and attempts == 1:
     print("You have successfully logged into your account!")
-    original_balance = 5000.50
-    print(f"Your current balance is ${original_balance:.2f}") # {:.2f} displays float value with 2 decimal places
-    transaction = input("Would you like to deposit or withdraw? ")
+    original_balance = 5000.50 # Current amount in account
+    print(f"Your current balance is ${original_balance:.2f}") # {:.2f} displays float value with 2 decimal places for cents
+    transaction = input("Would you like to deposit or withdraw? ") # input to allow users to deposit or withdraw money
     
     # If transaction is deposit
     if transaction == "deposit":
@@ -40,9 +41,9 @@ if code == pin and attempts == 1:
             print("Thank you for visiting Jeff's Bank!")
     
     else:
-        print("Sorry, you entered an invalid request!")
+        print("Sorry, you have entered an invalid request!")
 
-# Incorrext code in second attempt
+# Incorrect code in second attempt
 if code != pin and attempts == 2:
     code = int(input("Sorry, you have entered an incorrect PIN number. You have 1 more attempt. Please try again: "))
     attempts = attempts + 1
@@ -77,11 +78,11 @@ if code == pin and attempts == 2:
             print("Thank you for visiting Jeff's Bank!")
     
     else:
-        print("Sorry, you entered an invalid request!")
+        print("Sorry, you have entered an invalid request!")
 
-# Incorrect code in third attempt; account is locked
+# Incorrect code in third attempt; account is locked after 3 failed login attempts
 if code != pin and attempts == 3:
-    print("Your account has now been locked after 3 unsuccesssful attempts. Please try again later.")
+    print("Your account has been locked after 3 unsuccesssful login attempts. Please try again later.")
  
 # Correct code in third attempt
 if code == pin and attempts == 3:
@@ -111,3 +112,6 @@ if code == pin and attempts == 3:
             new_balance = original_balance - amount
             print(f"Your new balance is ${new_balance:.2f}")
             print("Thank you for visiting Jeff's Bank!")
+    
+    else:
+        print("Sorry, you have entered an invalid request!")
